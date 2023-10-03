31d9611738a1140c205b8102600145c71ae601a8
gohls <sagohl@gmail.com>
2023-10-02 18:48:01 -0700

    add divider

---
+++ src/components/common/Divider.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Divider.scss"; // Import your CSS file for styling
+
+const Divider = () => {
+  return (
+    <div className='divider-container'>
+      <div className='divider'></div>
+    </div>
+  );
+};
+
+export default Divider;
