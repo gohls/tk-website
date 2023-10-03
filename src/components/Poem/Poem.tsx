13999511d9f64ff689502f139df0bcb7df8d37b6
gohls <sagohl@gmail.com>
2023-10-02 22:58:32 -0700

    add author info and update style

--- src/components/Poem/Poem.tsx
+++ src/components/Poem/Poem.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import "./Poem.scss";
 
 interface PoemProps {
+  author: string;
   title: string;
   poem: string[];
 }
 
-const Poem = ({ title, poem }: PoemProps) => {
+const Poem = ({ author, title, poem }: PoemProps) => {
   return (
     <div className='poem-container'>
       <div className='poem-text'>
         <div className='poem-title'>{title}</div>
+        <div className='poem-author'>{author}</div>
         <>
           {poem.map((line, index) => {
             return <p key={index}>{line}</p>;
