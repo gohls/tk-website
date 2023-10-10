c4826e1c9576ebde0d726211addbcc51efb69011
gohls <sagohl@gmail.com>
2023-10-09 21:01:13 -0700

    update menu bar

--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -1,22 +1,30 @@
 import React from "react";
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import signature from "../../Tom-Kuhn--signature.png";
 
 function MenuBar() {
+  const navItems = ["Home", "About", "Contact"];
+
   return (
     <div className='App'>
-      <AppBar position='static'>
-        <Toolbar variant='dense'>
-          <IconButton
-            edge='start'
-            color='inherit'
-            aria-label='menu'
-            sx={{ mr: 2 }}
-          >
-            {/* <MenuIcon /> */}
-          </IconButton>
-          <Typography variant='h6' color='inherit' component='div'>
-            Photos
-          </Typography>
+      <AppBar
+        position='absolute'
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar className='toolbar' variant='dense'>
+          <img
+            className='image'
+            src={signature}
+            alt='Tom Kuhn signature'
+            width='100'
+          />
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: "black" }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
         </Toolbar>
       </AppBar>
     </div>
