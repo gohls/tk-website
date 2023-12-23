0d325f22b0b32c39df4f1c1abb85d817c7defe03
Simon Gohl <sagohl@gmail.com>
2023-12-23 13:06:24 -0800

    add Card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Card: React.FC<Props> = ({ children }) => {
+  return <StyledWrapper>{children}</StyledWrapper>;
+};
+
+export default Card;
+
+const StyledWrapper = styled.div`
+  border-radius: 1rem;
+  width: 100%;
+  background-color: ${({ theme }) =>
+    theme.scheme === "light" ? "white" : theme.colors.gray4};
+  @media (min-width: 768px) {
+    /* padding: 1rem; */
+  }
+  @media (min-width: 1024px) {
+    /* padding: 1rem; */
+  }
+`;
+
+// display: flex;
+// padding: 0.25rem;
+// flex-direction: column;
