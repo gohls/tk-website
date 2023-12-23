05ce04561f357b1b7ed5227ac987ae988cb9ebc0
Simon Gohl <sagohl@gmail.com>
2023-12-23 13:45:22 -0800

    use Paper component

--- src/pages/Detail/PoemDetail/index.tsx
+++ src/pages/Detail/PoemDetail/index.tsx
@@ -4,6 +4,7 @@ import PoemFooter from "./PoemFooter";
 import styled from "@emotion/styled";
 import { poems } from "src/text/poems";
 import PoemBody from "./PoemBody";
+import Paper from "src/components/Paper";
 
 type Props = {
   slug: string;
@@ -15,32 +16,14 @@ const PoemDetail: React.FC<Props> = ({ slug }) => {
   if (!idx) return null;
 
   return (
-    <StyledWrapper>
+    <Paper>
       <PoemHeader data={data[idx]} />
       <PoemBody data={data[idx]} />
       <>
         <PoemFooter />
       </>
-    </StyledWrapper>
+    </Paper>
   );
 };
 
 export default PoemDetail;
-
-const StyledWrapper = styled.div`
-  padding-left: 1.5rem;
-  padding-right: 1.5rem;
-  padding-top: 3rem;
-  padding-bottom: 3rem;
-  border-radius: 1.5rem;
-  max-width: 56rem;
-  background-color: ${({ theme }) =>
-    theme.scheme === "light" ? "white" : theme.colors.gray4};
-  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
-    0 2px 4px -1px rgba(0, 0, 0, 0.06);
-  margin: 0 auto;
-  > article {
-    margin: 0 auto;
-    max-width: 42rem;
-  }
-`;
