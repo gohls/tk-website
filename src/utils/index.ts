9195cd3d9d801ebd73060f8bd505e56092c2f83d
Simon Gohl <sagohl@gmail.com>
2023-12-25 15:38:29 -0800

    add TODO comment

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 const formatDate = (date: string | number | Date) => {
   const d = new Date(date);
+  // TODO: make option a param to pass in
   const res = d.toLocaleDateString(undefined, {
     year: "numeric",
     month: "short",
