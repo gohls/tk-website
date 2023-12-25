880d0d3ed4c6eb7586f70f6292f321465a8aacd0
Simon Gohl <sagohl@gmail.com>
2023-12-25 15:11:00 -0800

    udpate types

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,40 +5,21 @@ export interface routerType {
   element: any;
 }
 
-export type TPoemStatus = "Private" | "Public" | "PublicOnDetail";
-export type TPoemType = "Post" | "Paper" | "Page";
-
 export type TPoem = {
   id: string;
-
   date: { start_date: string };
-  type: TPoemType[];
   slug: string;
-  contents?: string[];
-  category?: string[];
-  poem?: string[];
-  author?: {
-    id: string;
-    name: string;
-    profile_photo?: string;
-  }[];
+  booklet?: string[];
+  poem: string[];
+  author: string;
   title: string;
-  status: TPoemStatus[];
   createdTime: string;
   fullWidth: boolean;
   thumbnail?: string;
-  isCover?: boolean;
 };
 
 export type PoemDetail = TPoem;
 
 export type TPoems = TPoem[];
-
-export type TContents = {
-  [contentName: string]: number;
-};
-export type TCategories = {
-  [category: string]: number;
-};
 
 export type ThemeType = "dark" | "light";
