c48212fd409cb21310c2f175d87f8c545cd87dec
Simon Gohl <sagohl@gmail.com>
2023-12-24 16:34:01 -0800

    bugfix: udpate CONFIG import path

--- src/layouts/Header/Logo.tsx
+++ src/layouts/Header/Logo.tsx
@@ -1,11 +1,11 @@
-import { WEB } from "src/constants/web";
+import { CONFIG } from "src/config";
 import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <StyledWrapper to="/" aria-label={WEB.site.title}>
-      {WEB.site.title}
+    <StyledWrapper to="/" aria-label={CONFIG.site.title}>
+      {CONFIG.site.title}
     </StyledWrapper>
   );
 };
