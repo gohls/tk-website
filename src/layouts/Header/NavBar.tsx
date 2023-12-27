713a5129bd701f5af0854c64097458cd4dd5fecc
Simon Gohl <sagohl@gmail.com>
2023-12-27 14:50:31 -0800

    remove unused imports

--- src/layouts/Header/NavBar.tsx
+++ src/layouts/Header/NavBar.tsx
@@ -1,6 +1,5 @@
 import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
-// import Link from "next/link";
 
 const NavBar: React.FC = () => {
   const links = [{ id: 1, name: "About", to: "/about" }];
