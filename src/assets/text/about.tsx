09c7c75549fb9ac7469189b96fa21a209b59c05c
Simon Gohl <sagohl@gmail.com>
2023-12-27 14:13:23 -0800

    add `about` info

---
+++ src/assets/text/about.tsx
@@ -0,0 +1,12 @@
+import { CONFIG } from "src/config";
+
+export const about = {
+  title: CONFIG.profile.name,
+  subtitle: CONFIG.profile.title,
+  about: [
+    "Poet Tom Kuhn, a distinguished sonneteer, seamlessly bridges the rich tapestry of poetic tradition with avant-garde exploration. His mastery lies not only in honoring the echoes of the past but in daring to propel the sonnet into uncharted realms, crafting a poetic legacy that stands at the intersection of tradition and innovation.",
+    "Immersed in the arts and the melodious cadence of music, Kuhn's creative tapestry is woven with threads of diverse influences. Nature, a perennial muse, adds a layer of timeless depth to his compositions. As a devoted student, he harmonizes the structured elegance of sonnets with the fluidity of artistic expression, creating a symphony of words that resonates with both the familiar and the unexplored.",
+    "Tom Kuhn emerges not just as a sonneteer but as a visionary poet, navigating the delicate balance between homage and evolution. His verses become a captivating narrative, where tradition meets exploration, and each carefully chosen word contributes to a harmonious blend of past, present, and future.",
+  ],
+  copyright: "Tom Kuhn Las Vegas Nevada July, 2023",
+};
