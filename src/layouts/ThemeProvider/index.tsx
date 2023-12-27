b35224c769355e4443059df8a224536d21c5ccfa
Simon Gohl <sagohl@gmail.com>
2023-12-27 14:53:50 -0800

    use init scheme state

--- src/layouts/ThemeProvider/index.tsx
+++ src/layouts/ThemeProvider/index.tsx
@@ -1,7 +1,7 @@
+import React from "react";
 import { ThemeProvider as _ThemeProvider } from "@emotion/react";
 import { Global } from "./Global";
 import { Scheme, createTheme } from "src/styles";
-import React from "react";
 
 const initialState = {
   scheme: "light",
@@ -15,7 +15,7 @@ type Props = {
 };
 
 const ThemeProvider = ({ children }: Props) => {
-  const [scheme, setScheme] = React.useState("light");
+  const [scheme, setScheme] = React.useState(initialState.scheme);
 
   const toggle = (scheme: Scheme) => {
     setScheme(scheme);
