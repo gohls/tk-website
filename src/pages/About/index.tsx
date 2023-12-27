1b5ff51a3f08743f6c5d43d9de0ffac2c63c8d1c
Simon Gohl <sagohl@gmail.com>
2023-12-26 19:51:45 -0800

    WIP: styling

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,8 +1,23 @@
+import styled from "@emotion/styled";
 import React from "react";
+import NavToBtn from "src/components/NavToBtn";
 import Paper from "src/components/Paper";
+import { CONFIG } from "src/config";
+import Main from "src/layouts/Main";
 
 const About = () => {
-  return <Paper>Hello About</Paper>;
+  return (
+    <StyledWrapper>
+      <Paper>
+        <h1 className="title">{CONFIG.profile.name}</h1>
+        Hello About
+      </Paper>
+    </StyledWrapper>
+  );
 };
 
 export default About;
+
+const StyledWrapper = styled.div`
+  padding: 2rem 0;
+`;
