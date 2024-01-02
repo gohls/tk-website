f3aa1989cd234e60ab18a82de7c652e0378030b1
Simon Gohl <sagohl@gmail.com>
2024-01-01 19:15:15 -0800

    wip: graphql impl

--- server/app.py
+++ server/app.py
@@ -1,14 +1,31 @@
-from flask import Flask, send_from_directory
-from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS #comment this on deployment
-from api.HelloApiHandler import HelloApiHandler
+from ariadne.constants import PLAYGROUND_HTML
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from ariadne import QueryType, load_schema_from_path, make_executable_schema, graphql_sync
+from datasource import poems_data
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-CORS(app) #comment this on deployment
-api = Api(app)
+# schema
+type_defs = load_schema_from_path("schema.gql")
 
-@app.route("/", defaults={'path':''})
-def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
+# Initialize query
+query = QueryType()
 
-api.add_resource(HelloApiHandler, '/flask/hello')
+# resolver
+@query.field("poems")
+def poems(*_):
+   return poems_data
+
+schema = make_executable_schema(type_defs, query)
+
+app = Flask(__name__, static_url_path='', static_folder='client/build')
+CORS(app)
+
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+   data = request.get_json()
+   success, result = graphql_sync(schema, data, context_value={"request": request})
+   status_code = 200 if success else 400
+   return jsonify(result), status_code
+
+if __name__ == "__main__":
+   app.run(debug=True)
