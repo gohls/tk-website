84059cac2e90312abb454e4ab4b187f41140ee43
Simon Gohl <sagohl@gmail.com>
2024-01-02 21:15:16 -0800

    add `usePoemsQuery` hook

--- client/src/hooks/usePoemQuery.ts
+++ client/src/hooks/usePoemQuery.ts
@@ -1,11 +1,25 @@
 import { useQuery } from "react-query";
-import { useParams } from "react-router-dom";
+import { request, gql } from "graphql-request";
 
-const usePostQuery = () => {
-  const { poemId } = useParams();
-  const { data } = useQuery({});
+const endpoint = "/graphql";
+const POEM_QUERY = gql`
+  {
+    poems {
+      title
+      author
+      poem
+    }
+  }
+`;
+
+const usePoemsQuery = () => {
+  const data = useQuery("poems", () => {
+    return request(endpoint, POEM_QUERY);
+  });
+
+  if (!data) throw new Error("Poem data is not found");
 
   return data;
 };
 
-export default usePostQuery;
+export default usePoemsQuery;
